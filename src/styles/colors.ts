aa80534029cdc4f455bfaf406fd06ceea17dc8f2
Antoj2000 <g00385306@atu.ie>
2025-11-09 03:58:40 +0000

    style files and refactored existing screens

---
+++ src/styles/colors.ts
@@ -0,0 +1,13 @@
+// Stylesheet for colors used throughout the project
+
+export const Colors = { 
+    dpdRed: '#ff0000ff',
+    accent: '#a5a5a5ff',
+    blackText: '#000',
+    whiteText: '#fff',
+    mutedText: '#555',
+    border: '#CCC',
+    lightGray: '#F9F9F9',
+    background: '#FFF',
+    overlay: 'rgba(0, 0, 0, 0.5)', // RGBA used to create a transparent overlay
+};

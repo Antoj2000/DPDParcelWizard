62bd0d7ddc4a1c635002a6178953947f89ebf42d
Antoj2000 <g00385306@atu.ie>
2026-03-08 03:19:11 +0000

    Added some inputs for the login

--- app/(tabs)/playground.tsx
+++ app/(tabs)/playground.tsx
@@ -1,15 +1,37 @@
 import { ScrollView, StyleSheet } from "react-native";
 import LoginToggle from "@/components/login/LoginToggle";
 import LoginCard from "@/components/login/LoginCard";
+import LoginInput from "@/components/login/LoginInput";
 import { useState } from "react";
 
 export default function Playground() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [loginState, setLoginState] = useState("login");
+  const [hidePassword, setHidePassword] = useState(true);
 
   return (
     <ScrollView style={styles.container}>
       <LoginCard>
         <LoginToggle value={loginState} onChange={setLoginState} />
+        <LoginInput
+          label="Email"
+          placeholder="Enter your email"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+          autoCompleteType="email"
+        />
+        <LoginInput
+          label="Password"
+          placeholder="Enter your password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={hidePassword}
+          rightIcon={hidePassword ? "eye-off-outline" : "eye-outline"}
+          onRightPress={() => setHidePassword((prev) => !prev)}
+          autoCompleteType="password"
+        />
       </LoginCard>
     </ScrollView>
   );
